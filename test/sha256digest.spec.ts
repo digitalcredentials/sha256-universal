d34c62bb186746e32db33c72c3a8adfc54e02435
Dmitri Zagidulin <dzagidulin@gmail.com>
2022-12-13 20:01:11 -0500

    Add support for hashing byte arrays.

--- test/sha256digest.spec.ts
+++ test/sha256digest.spec.ts
@@ -2,7 +2,8 @@ import { expect } from 'chai'
 import { sha256digest } from '../src'
 
 describe('sha256digest', () => {
-  it('calls function', async () => {
-    expect(await sha256digest('abc')).to.exist
+  it('hashes strings or bytes', async () => {
+    expect(await sha256digest('test')).to
+      .eql(await sha256digest(new TextEncoder().encode('test')))
   })
 })
